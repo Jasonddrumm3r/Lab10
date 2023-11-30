8e356e3cd1040f697170af634de0e3a4350f5805
jason <jdelam@umich.edu>
2023-11-30 18:36:22 -0500

    Fix Selection Sort

--- src/SelectionSort/SelectionSort.java
+++ src/SelectionSort/SelectionSort.java
@@ -19,7 +19,7 @@ public class SelectionSort {
 				if (x[i] > x[j]) {
 					temp = x[i];
 					x[i] = x[j];
-					temp = x[j];
+					x[j] = temp; 	//Switch temp = x[j]; to x[j] = temp;
 				}
 			} // end of inner for loop
 		} // end of outer for loop
